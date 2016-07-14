3395f0f25de4f420c467fc73568e898880425d2e
vcinust <vincechan615@gmail.com>
2016-07-14 19:15:51 +0800

    update funny 3

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package com.vogella.git.first;
 
 public class GitTest {
   public static void main(String[] args) {
-    System.out.println("Git is funny");
+    System.out.println("Git is funny 3");
   }
 } 
