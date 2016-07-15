4e0a8f8b210011ee5830ffcdfb1cc07d30400223
vcinust <vincechan615@gmail.com>
2016-07-15 15:25:04 +0800

    Good commit
    Conflicts:
    	com.vogella.git.first/src/com/vogella/git/first/GitTest.java

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -3,5 +3,6 @@ package com.vogella.git.first;
 public class GitTest {
   public static void main(String[] args) {
     System.out.println("Git is funny 3");
+    System.out.println("Good");
   }
 } 
