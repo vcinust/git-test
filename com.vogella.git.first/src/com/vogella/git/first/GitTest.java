355c342c20ff1e03caf6d77888cd32a828759889
Vince Chan <vincechan615@gmail.com>
2020-04-23 00:59:54 +0800

    test rebase (+1 squashed commits)
    Squashed commits:
    
    [5e3ad63] test rebase

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -3,6 +3,6 @@ package com.vogella.git.first;
 public class GitTest {
   public static void main(String[] args) {
     System.out.println("Git is funny 3");
-    System.out.println("Good");
+    System.out.println("Good Good Good");
   }
 } 
